a88c29becb69d66712a3bd4559707e4dbee1b7f6
mansoor <mansoor@mansoor-CQ3659IN>
2013-11-25 22:26:34 +0530

    isecure

--- src/main/java/app/secure/isecure/Login4Users.java
+++ src/main/java/app/secure/isecure/Login4Users.java
@@ -21,84 +21,81 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
  * @author mansoor
  *
  */
-public class Login4Users implements LoginModule,LogoutHandler,LogoutSuccessHandler, Serializable {
+public class Login4Users implements LoginModule, LogoutHandler, LogoutSuccessHandler, Serializable {
 
-	/**
-	 * 
-	 */
-    
+    /**
+     *
+     */
     private String username;
     private String password;
     private Subject subject;
-    
-    
-    
-	public Login4Users() {
-		// TODO Auto-generated constructor stub
-	}
 
-	/* (non-Javadoc)
-	 * @see javax.security.auth.spi.LoginModule#initialize(javax.security.auth.Subject, javax.security.auth.callback.CallbackHandler, java.util.Map, java.util.Map)
-	 */
-	@Override
-	public void initialize(Subject subject, CallbackHandler callbackHandler,
-			Map<String, ?> sharedState, Map<String, ?> options) {
-		// TODO Auto-generated method stub
-	     
-	         this.subject = subject;
-	  
-	         try {
-	             NameCallback nameCallback = new NameCallback("prompt");
-	             PasswordCallback passwordCallback = new PasswordCallback("prompt",
-	                     false);
-	  
-	             callbackHandler.handle(new Callback[] { nameCallback,
-	                     passwordCallback });
-	  
-	             password = new String(passwordCallback.getPassword());
-	             username = nameCallback.getName();
-                     
-	         } catch (Exception e) {
-	             throw new RuntimeException(e);
-	         }
+    public Login4Users() {
+        // TODO Auto-generated constructor stub
+    }
 
-	}
+    /* (non-Javadoc)
+     * @see javax.security.auth.spi.LoginModule#initialize(javax.security.auth.Subject, javax.security.auth.callback.CallbackHandler, java.util.Map, java.util.Map)
+     */
+    @Override
+    public void initialize(Subject subject, CallbackHandler callbackHandler,
+            Map<String, ?> sharedState, Map<String, ?> options) {
+        // TODO Auto-generated method stub
 
-	/* (non-Javadoc)
-	 * @see javax.security.auth.spi.LoginModule#login()
-	 */
-	@Override
-	public boolean login() throws LoginException {
-		subject.getPrincipals().add(new UserPrincipal("ROLE_USER"));
-		return true;
-	}
+        this.subject = subject;
 
-	/* (non-Javadoc)
-	 * @see javax.security.auth.spi.LoginModule#commit()
-	 */
-	@Override
-	public boolean commit() throws LoginException {
-		// TODO Auto-generated method stub
-		return true;
-	}
+        try {
+            NameCallback nameCallback = new NameCallback("prompt");
+            PasswordCallback passwordCallback = new PasswordCallback("prompt",
+                    false);
 
-	/* (non-Javadoc)
-	 * @see javax.security.auth.spi.LoginModule#abort()
-	 */
-	@Override
-	public boolean abort() throws LoginException {
-		// TODO Auto-generated method stub
-		return false;
-	}
+            callbackHandler.handle(new Callback[]{nameCallback,
+                        passwordCallback});
 
-	/* (non-Javadoc)
-	 * @see javax.security.auth.spi.LoginModule#logout()
-	 */
-	@Override
-	public boolean logout() throws LoginException {
-		// TODO Auto-generated method stub
-		return false;
-	}
+            password = new String(passwordCallback.getPassword());
+            username = nameCallback.getName();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /* (non-Javadoc)
+     * @see javax.security.auth.spi.LoginModule#login()
+     */
+    @Override
+    public boolean login() throws LoginException {
+        subject.getPrincipals().add(new UserPrincipal("ROLE_USER"));
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.security.auth.spi.LoginModule#commit()
+     */
+    @Override
+    public boolean commit() throws LoginException {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.security.auth.spi.LoginModule#abort()
+     */
+    @Override
+    public boolean abort() throws LoginException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.security.auth.spi.LoginModule#logout()
+     */
+    @Override
+    public boolean logout() throws LoginException {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
@@ -109,5 +106,4 @@ public class Login4Users implements LoginModule,LogoutHandler,LogoutSuccessHandl
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
 }
